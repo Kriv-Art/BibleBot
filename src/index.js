bf1cf33c4a4eb6280ea51461fd42b0cceb5b595e
Noah Too <krivahtoo@gmail.com>
2020-01-13 23:29:18 +0300

    Added a more understandable in start command

--- src/index.js
+++ src/index.js
@@ -21,9 +21,11 @@ bot.use(session())
 
 bot.start(({ replyWithMarkdown }) => replyWithMarkdown(
   'I can send you bible verse\n' +
-  'Just send me a verse\n' +
+  'Just send me a verse or text with a verse\n' +
   'e.g.\n' +
-  '  `John 3:16` `Jn3:16`'
+  '  `John 3:16` or `Jn3:16`\n' +
+  '   or\n' +
+  '`Some messsage with a Mt4.4 verse in between text`'
 ))
 bot.help(({ reply }) => reply('Comming Soon'))
 bot.settings(({ reply }) => reply('Comming Soon'))
